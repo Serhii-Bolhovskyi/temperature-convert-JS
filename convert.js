49c562a4a8b7963d5558a5519d0ae2dd14e1884d
Serhii Bolhovskyi <serhiibolhovskyi@MacBook-Air-Serhii.local>
2024-02-19 19:58:10 +0100

    Created

---
+++ convert.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form');
+const celsius = document.getElementById('celsiusInput');
+const fahrenheit = document.getElementById('fahrenheitInput');
+
+celsius.oninput = function () { 
+    let f = (parseFloat(celsius.value) * 9) / 5 + 32; 
+    fahrenheit.value = parseFloat(f.toFixed(2)); 
+}
+
+fahrenheit.oninput = function () { 
+    let c = ((parseFloat( 
+        fahrenheit.value) - 32) * 5) / 9; 
+    celsius.value = parseFloat(c.toFixed(2)); 
+} 
